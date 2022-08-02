5cff6d871c2b2d529815ead884283f8c9406596e
Julito Rojas <jrojasg1@eafit.edu.co>
2022-08-02 09:51:16 -0500

    cambios en el view  home

--- movie/views.py
+++ movie/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html',{'name':'Greg Lim'})
-    
+    return render(request,'home.html',{'name':'Julito'})
+
 
 
 
