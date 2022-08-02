c4c72576ab2fbad875de2672e77753cd89ad1796
Julito Rojas <jrojasg1@eafit.edu.co>
2022-08-02 10:17:59 -0500

    about y colorcito

--- movie/views.py
+++ movie/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'home.html',{'name':'Julito'})
 
+def about(request):
+    return render(request,'about.html')
+
 
 
 
