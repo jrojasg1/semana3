20b20e42e264518d436127f08d89df1cd0c3d739
Julito Rojas <jrojasg1@eafit.edu.co>
2022-11-01 10:25:45 -0500

    “ocommit2”

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html',{'name':'Julito'})
+    return render(request,'home.html',{'name':'Luis fernandinho'})
 
 def about(request):
     return render(request,'about.html')
